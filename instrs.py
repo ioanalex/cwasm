99adbcae2e6b0518bbb0f9fd8698f0fbba29ea38
ioanalex <ioan.alex96@gmail.com>
2019-12-18 03:43:46 +0200

    Fix: Fixed bug in instrs.py that caused wrong C code due to missing break statements

--- instrs.py
+++ instrs.py
@@ -22,54 +22,56 @@ while i < len(ops_lines):
     print("case {}:\nname = \"{}\";".format(opcode, name));
 
     # else and end commands have no type so we are done with them
-    if(name == "else" or name == "end"):
-        i += 3
-        continue
+    if(not name == "else" and not name == "end"):
+        # now we have to parse the type signatures that appear on line i+2
+        # so let's define a function that take the line and gives back two strings
+        # one with the args and one with the ret
 
-    # now we have to parse the type signatures that appear on line i+2
-    # so let's define a function that take the line and gives back two strings
-    # one with the args and one with the ret
+        def parse_args(line):
+            if line == "":
+                return "",""
+            args, ret = line.split('→');
+            args = args.strip("[]")
+            ret = ret.strip("[]")
+            return args, ret
 
-    def parse_args(line):
-        if line == "":
-            return "",""
-        args, ret = line.split('→');
-        args = args.strip("[]")
-        ret = ret.strip("[]")
-        return args, ret
-
-    args, ret = parse_args(ops_lines[i+2])
-    
-    # Now that we have args and ret as strings with no brackets etc we need to map
-    # them to push_back statements. Let's write a function that take a type string 
-    # and returns a list of itypes
-
-    def get_itypes(args, field_name):
-        types = args.split(" ")
-        # Now we need a functions that reads a type and return the push_back statement
-        def statement_for_type(type):
-            if type == "":
-                return ""
-            s = "type.{}.push_back".format(field_name)
-            if type[0] == 't':
-                # we have a polymorphic type
-                if len(type) > 1:
-                    if type[1] == '∗':
-                        if len(type) == 3:
-                            return s + "(ptype({}, true, false));".format(type[2])
-                        return s + "(ptype(0, true, false));"
-                    if type[1] == '?':
-                        if len(type) == 3:
-                            return s + "(ptype({}, false, true));".format(type[2])
-                        return s + "(ptype(0, false, true));"
-                    return s + "(ptype({}, false, false));".format(type[1])
-                return s + "(ptype(0, false, false));"
-            return s + "(type::Value::{});".format(type)
+        args, ret = parse_args(ops_lines[i+2])
         
-        return list(map(statement_for_type, types))
-        # for type in types:
-        #     print(statement_for_type(type))
+        # Now that we have args and ret as strings with no brackets etc we need to map
+        # them to push_back statements. Let's write a function that take a type string 
+        # and returns a list of itypes
 
+        def get_itypes(args, field_name):
+            types = args.split(" ")
+            # Now we need a functions that reads a type and return the push_back statement
+            def statement_for_type(type):
+                if type == "":
+                    return ""
+                s = "type.{}.push_back".format(field_name)
+                if type[0] == 't':
+                    # we have a polymorphic type
+                    if len(type) > 1:
+                        if type[1] == '∗':
+                            if len(type) == 3:
+                                return s + "(ptype({}, true, false));".format(type[2])
+                            return s + "(ptype(0, true, false));"
+                        if type[1] == '?':
+                            if len(type) == 3:
+                                return s + "(ptype({}, false, true));".format(type[2])
+                            return s + "(ptype(0, false, true));"
+                        return s + "(ptype({}, false, false));".format(type[1])
+                    return s + "(ptype(0, false, false));"
+                return s + "(type::Value::{});".format(type)
+            
+            return list(map(statement_for_type, types))
+            # for type in types:
+            #     print(statement_for_type(type))
+        i+=4
+    else:
+        args = ""
+        ret = ""
+        i+=3
+        
     args = get_itypes(args, "args")
     ret = get_itypes(ret, "ret")
 
@@ -81,4 +83,4 @@ while i < len(ops_lines):
 
 
     print("break;")
-    i+=4
+    
