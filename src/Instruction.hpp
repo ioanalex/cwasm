7a296a36d7bc84539faa68161912d18e544cc5e6
ioanalex <ioan.alex96@gmail.com>
2020-01-27 14:37:06 +0200

    WIP: Instructions, defined basic ImmediateInstr class

--- src/Instruction.hpp
+++ src/Instruction.hpp
@@ -5,19 +5,20 @@
 #include "types.hpp"
 
 class InstrImpl {
+    private:
+        byte opcode; 
+        u32 pos; // Index in bytecode
     // this is the abstract class for all different types of instructions
     public:
+    
         virtual void run   (/* takes a Store   */)=0;
         virtual void verify(/* takes a Context */)=0;
 };
 
 
 class Instruction {
-    public:
-        InstrProfile *profile;
-        InstrImpl *impl; 
-        u32 pos; // Index in bytecode
-
+    public:        
+        InstrImpl *impl;  
         // Constructor
         Instruction(InstrProfile *, InstrImpl *, u32);
 
@@ -26,10 +27,15 @@ class Instruction {
         // desides which sub-class to use as an InstrImpl
         // initializes the impl onject
         // initializes the instr object using basic constructor
-        static Instruction createInstr(byte *, u32 *);
+        static Instruction createInstr(byte *, u32 *); // called when parsing
 
         void run   (/* takes a Store   */); // calls impl -> run()
         void verify(/* takes a Context */); // calls impl -> verify()
+};
+
+template<class t>
+class ImmediateImpl: public InstrImpl {
+    T imm;
 };
 
 
